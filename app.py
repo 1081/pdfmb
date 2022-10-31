0f36ace7d8bcc5e3493c691487b62ab60588f0ba
Jan Sattler <jan1081@pm.me>
2022-10-31 23:09:13 +0100

    ui extended

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import dearpygui.demo as demo
 from pathlib import Path
 import pdfmb
 import platform
-from time import sleep
-import subprocess
 from subprocess import call
+from dataclasses import dataclass, field
+
+# example pdfs/file1.pdf
+# example pdfs
 
 
 def macos_settings():
@@ -26,185 +28,321 @@ def _log(sender, app_data, user_data):
     print(f"sender: {sender}, \t app_data: {app_data}, \t user_data: {user_data}")
 
 
-def update_show_pdfs(sender, app_data, user_data):
-    pass
-
-
-def update_path_to_output():
-    if not dpg.get_value("custom folder"):
-        pass
-
-
-def search_folder(sender, folder, user_data):
-    dpg.show_item("search folder indicator")
-    if folder:
-        if Path(folder).is_dir():
-            pdfs = sorted(Path(folder).rglob("*.pdf"))
-            dpg.set_item_label("show pdfs", label=f"show PDFs ({len(pdfs)})")
-            dpg.hide_item("search folder indicator")
-            update_path_to_output()
-            return
-    dpg.set_item_label("show pdfs", label="no valid path")
-    dpg.hide_item("search folder indicator")
-
-
-def open_output_folder():
-    targetDirectory = "~/Desktop"
-    targetDirectory = ""
-    call(["open", targetDirectory])
-    # subprocess.Popen(r'explorer /select,"C:\path\of\folder\file"')
-
-
-def show_existing_pdf(sender, app_data, user_data):
-    if app_data:
-        dpg.show_item("group existing pdf")
-    else:
-        dpg.hide_item("group existing pdf")
-
-
-def custom_filename(sender, app_data, user_data):
-    if app_data:
-        dpg.show_item("info timestamp")
-    else:
-        dpg.hide_item("info timestamp")
-
+# ------------------------------------------------------------------------------
 
 dpg.create_context()
 
-macos_settings()
+# ------------------------------------------------------------------------------
 
+macos_settings()
 spacer_height = 15
 
+# ------------------------------------------------------------------------------
 
-with dpg.window(tag="Primary Window"):
-    dpg.add_text("Merge PDF files with bookmarks")
-    # dpg.add_radio_button(
-    #     (
-    #         "Create new PDF",
-    #         "Add to an existing PDF (the original file will not be modified)",
-    #     ),
-    #     callback=show_existing_pdf,
-    #     horizontal=True,
-    # )
-    # dpg.add_spacer(height=spacer_height)
-    dpg.add_text("")
-    dpg.add_text("PDF files")
-    with dpg.group(horizontal=True):
-        dpg.add_input_text(
-            tag="source folder",
-            hint="Path to folder",
-            callback=search_folder,
-        )
-        dpg.add_button(
-            tag="show pdfs",
-            label="PDFs (0)",
-            width=130,
-        )
-        dpg.add_loading_indicator(
-            radius=1.4,
-            style=1,
-            color=(150, 255, 150, 255),
-            tag="search folder indicator",
-            show=False,
-        )
-    # dpg.add_spacer(height=spacer_height)
-    dpg.add_text("")
-    dpg.add_checkbox(
-        tag="add to existing pdf",
-        label="Add to an existing PDF",
-        callback=show_existing_pdf,
-    )
+primary_window = dpg.generate_uuid()
+#
+source_folder = dpg.generate_uuid()
+source_folder_pdfs = dpg.generate_uuid()
+source_folder_loading_indicator = dpg.generate_uuid()
+#
+check_add = dpg.generate_uuid()
+group_existing_pdf = dpg.generate_uuid()
+existing_pdf = dpg.generate_uuid()
+existing_pdf_status = dpg.generate_uuid()
+#
+output_folder = dpg.generate_uuid()
+output_folder_status = dpg.generate_uuid()
+#
+output_filename = dpg.generate_uuid()
+timestamp_info = dpg.generate_uuid()
+#
+check_flatten = dpg.generate_uuid()
+#
+create_pdf = dpg.generate_uuid()
+create_pdf_feedback = dpg.generate_uuid()
+create_pdf_loading_indicator = dpg.generate_uuid()
+#
+goto_output_folder = dpg.generate_uuid()
+open_pdf = dpg.generate_uuid()
 
-    # dpg.add_spacer(height=spacer_height)
+# ------------------------------------------------------------------------------
 
-    with dpg.group(tag="group existing pdf", show=False):
-        # dpg.add_text("Existing PDF")
-        with dpg.group(horizontal=True):
-            dpg.add_input_text(
-                tag="existing pdf",
-                hint="Path to existing PDF",
-                callback=_log,
-            )
-            dpg.add_button(
-                label="Status ok",
-                width=130,
-            )
 
-    # dpg.add_spacer(height=spacer_height)
-    dpg.add_text("")
+@dataclass
+class Model:
+    source_folder: Path = None
+    pdfs: list[int] = field(default_factory=list)
+    add: bool = False
+    existing_pdf: Path = None
+    output_folder: Path = None
+    output_filenname: str = None
+    flatten: bool = False
 
-    dpg.add_text("Output")
-    with dpg.group(horizontal=True):
-        dpg.add_input_text(
-            tag="output folder",
-            hint="Path to output folder",
-            callback=_log,
-        )
-        dpg.add_checkbox(
-            tag="custom output folder",
-            label="Custom output folder",
-            callback=_log,
-        )
-    with dpg.group(horizontal=True):
-        dpg.add_input_text(
-            tag="output file name",
-            hint="File name",
-            callback=_log,
-        )
-        dpg.add_checkbox(
-            tag="custom  file name",
-            label="Custom file name",
-            callback=custom_filename,
-        )
-    dpg.add_text(
-        "A unique timestamp is automatically appended to the file name",
-        tag="info timestamp",
-        show=False,
-    )
-    # dpg.add_spacer(height=spacer_height)
-    dpg.add_text("")
+    def find_pdfs(self, folder):
+        if folder and Path(folder).is_dir():
+            self.pdfs = sorted(Path(folder).rglob("*.pdf"))
+            self.source_folder = Path(folder).absolute()
+        else:
+            self.pdfs = []
+            self.source_folder = False
+        return self.pdfs
 
-    dpg.add_checkbox(
-        tag="flatten structure",
-        label="Flatten folder structure in bookmark outline",
-    )
-    # dpg.add_spacer(height=spacer_height)
-    dpg.add_text("")
+    def set_existing_pdf(self, file: str):
+        if file and Path(file).is_file() and file.endswith(".pdf"):
+            self.existing_pdf = Path(file).absolute()
+        else:
+            self.existing_pdf = False
+        return self.existing_pdf
 
-    with dpg.group(horizontal=True):
-        dpg.add_button(
-            tag="create pdf",
-            label="Create PDF",
-            width=150,
-            callback=_log,
-        )
-        dpg.add_text("PDF file created with 123 pages and 12 bookmakrs")
+    def set_output_folder(self, folder):
+        if folder and Path(folder).is_dir():
+            self.output_folder = Path(folder).absolute()
+        else:
+            self.output_folder = self.default_output_folder
+        return self.output_folder
 
-    dpg.add_button(
-        tag="go to output file",
-        label="Go to output file",
-        width=150,
-        callback=open_output_folder,
-    )
+    def set_output_filename(self):
+        if self.add and self.existing_pdf:
+            return "added"
+            # return self.existing_pdf.with_stem(f"{existing_pdf.stem} - PDFs added")
+        else:
+            return "PDFs merged"
 
-    # demo.show_demo()
+    @property
+    def default_output_folder(self):
+        desktop = Path.home() / "Desktop"
+        return desktop.absolute()
 
-    # merge
+    @property
+    def default_output_filename(self):
+        return "PDFs merged"
+
+
+def x():
+    print(model)
+
+
+model = Model()
+
+# -------------------------------------------------------------------------------
+
+
+def changed_source_folder(sender, folder, user_data):
+    dpg.show_item(source_folder_loading_indicator)
+
+    pdfs = model.find_pdfs(folder)
+    if pdfs:
+        dpg.set_item_label(source_folder_pdfs, label=f"PDFs ({len(pdfs)})")
+        dpg.enable_item(source_folder_pdfs)
+    else:
+        dpg.set_item_label(source_folder_pdfs, label="no valid path")
+        dpg.disable_item(source_folder_pdfs)
+
+    dpg.hide_item(source_folder_loading_indicator)
+
+
+def clicked_source_folder_pdfs():
+    # TODO popup window
+    for p in model.pdfs:
+        print(p)
+
+
+def update_check_add(sender, checked, user_data):
+    if checked:
+        model.add = True
+        dpg.show_item(group_existing_pdf)
+
+    else:
+        model.add = False
+        dpg.hide_item(group_existing_pdf)
+    dpg.set_value(output_filename, value=model.set_output_filename())
+
+
+def changed_existing_pdf(sender, file, user_data):
+    if model.set_existing_pdf(file):
+        dpg.set_item_label(existing_pdf_status, label="ok")
+    else:
+        dpg.set_item_label(existing_pdf_status, label="not found")
+    dpg.set_value(output_filename, value=model.set_output_filename())
+
+
+def changed_output_folder(sender, folder, user_data):
+    if model.set_output_folder(folder):
+        # dpg.set_item_label(output_folder_status, label="ok")
+        dpg.set_value(output_folder_status, "ok")
+    else:
+        # dpg.set_item_label(output_folder_status, label="not found")
+        dpg.set_value(output_folder_status, "not found")
+
+
+def changed_output_filename(sender, filename, user_data):
+    pass
+
+
+def clicked_create_pdf():
+    pass
     # pdfmb.merge_from_folder(
     #     source_folder=Path("example pdfs"),
     #     output_folder=Path("output"),
     # )
 
-    # add
     # pdfmb.add_from_folder(
     #     source_folder=Path("example pdfs"),
     #     existing_pdf=Path("example pdfs/file1.pdf"),
     #     add_flat_hierachy=True,
     # )
 
+
+def clicked_goto_output_folder():
+    call(["open", model.output_folder.absolute()])
+
+
+def clicked_open_pdf():
+    pass
+
+
+def clicked_debug():
+    print("")
+    print(model)
+    print("")
+
+
+# ------------------------------------------------------------------------------
+
+with dpg.window(tag=primary_window):
+    dpg.add_text("Merge PDF files with bookmarks")
+    dpg.add_text("")
+
+    # ------------------------------------------------------------------------------
+
+    dpg.add_text("PDF files")
+    with dpg.group(horizontal=True):
+        dpg.add_input_text(
+            tag=source_folder,
+            hint="Path to folder",
+            callback=changed_source_folder,
+        )
+        dpg.add_button(
+            tag=source_folder_pdfs,
+            label="no valid path",
+            width=100,
+            enabled=False,
+            callback=clicked_source_folder_pdfs,
+        )
+        dpg.add_loading_indicator(
+            tag=source_folder_loading_indicator,
+            radius=1.4,
+            style=1,
+            color=(150, 255, 150, 255),
+            show=False,
+        )
+    dpg.add_text("")
+
+    # ------------------------------------------------------------------------------
+
+    dpg.add_checkbox(
+        tag=check_add,
+        label="Add to existing PDF",
+        callback=update_check_add,
+    )
+    with dpg.group(tag=group_existing_pdf, show=False):
+        with dpg.group(horizontal=True):
+            dpg.add_input_text(
+                tag=existing_pdf,
+                hint="Path to existing PDF",
+                callback=changed_existing_pdf,
+            )
+            dpg.add_button(
+                tag=existing_pdf_status,
+                label="no valid path",
+                width=100,
+                enabled=False,
+            )
+    dpg.add_text("")
+
+    # ------------------------------------------------------------------------------
+
+    dpg.add_text("Output")
+    with dpg.group(horizontal=True):
+        dpg.add_input_text(
+            tag=output_folder,
+            default_value=model.default_output_folder,
+            hint="Path to output folder",
+            callback=changed_output_folder,
+        )
+        dpg.add_text(
+            "",
+            tag=output_folder_status,
+        )
+    with dpg.group(horizontal=True):
+        dpg.add_input_text(
+            tag=output_filename,
+            hint="File name",
+            default_value=model.default_output_filename,
+            callback=changed_output_filename,
+        )
+        dpg.add_text(".pdf")
+
+    dpg.add_text(
+        "A unique timestamp is automatically appended to the file name",
+        tag=timestamp_info,
+        show=False,
+        color=(130, 130, 130),
+    )
+    dpg.add_text("")
+
+    # ------------------------------------------------------------------------------
+
+    dpg.add_checkbox(
+        tag=check_flatten,
+        label="Flatten folder structure in bookmark outline",
+    )
+    dpg.add_text("")
+
+    # ------------------------------------------------------------------------------
+
+    with dpg.group(horizontal=True):
+        dpg.add_button(
+            tag=create_pdf,
+            label="Create PDF",
+            width=150,
+            callback=clicked_create_pdf,
+        )
+        dpg.add_text(
+            "PDF file created with 123 pages and 12 bookmakrs",
+            tag=create_pdf_feedback,
+        )
+
+    dpg.add_text("")
+    dpg.add_button(
+        tag=goto_output_folder,
+        label="Open output",
+        width=150,
+        callback=clicked_goto_output_folder,
+    )
+    dpg.add_button(
+        tag=open_pdf,
+        label="Open PDF",
+        width=150,
+        callback=clicked_open_pdf,
+    )
+
+    # ------------------------------------------------------------------------------
+
+    dpg.add_text("")
+    dpg.add_button(
+        tag="debug",
+        label="debug",
+        width=300,
+        callback=clicked_debug,
+    )
+
+    # demo.show_demo()
+
+
 dpg.create_viewport(title="pdfmb", width=900, height=550)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
+dpg.set_primary_window(primary_window, True)
 dpg.start_dearpygui()
 dpg.destroy_context()
